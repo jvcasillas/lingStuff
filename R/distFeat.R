5e264294df447a2508b18f6c5bc277228507c9e4
jvcasill <casillas@Josephs-MBP-2.fios-router.home>
2016-12-26 16:20:22 -0500

    upload distFeat dataframe

---
+++ R/distFeat.R
@@ -0,0 +1,12 @@
+#' Distinctive features of Spanish allphones
+#'
+#' A data frame containing +/-/0 specifications for the distinctive 
+#' features of Spanish allophones. Based on the table by Casillas 
+#' & Bessett (2013) published in Cedeño, R. A. N., Colina, S., & 
+#' Bradley, T. G. (Eds.). Fonología generativa contemporánea de la 
+#' lengua española. Georgetown University Press.
+#'
+#'
+#' @format A data frame with 51 rows and 26 variables:
+#' @source \url{http://www.jvcasillas.com/}
+"distFeat"
