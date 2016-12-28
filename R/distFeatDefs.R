292c1580b8e27ba4d7594908095afa41114ebf5d
jvcasill <casillas@Josephs-MBP-2.fios-router.home>
2016-12-27 22:04:15 -0500

    add distinctive features definitions

---
+++ R/distFeatDefs.R
@@ -0,0 +1,12 @@
+#' Definitions of distinctive features 
+#'
+#' A data frame containing definitions for the distinctive 
+#' features of Spanish allophones. Supplement to the table by Casillas 
+#' & Bessett (2013) published in Cedeño, R. A. N., Colina, S., & 
+#' Bradley, T. G. (Eds.). Fonología generativa contemporánea de la 
+#' lengua española. Georgetown University Press.
+#'
+#'
+#' @format A data frame with 51 rows and 26 variables:
+#' @source \url{http://www.jvcasillas.com/}
+"distFeatDefs"
